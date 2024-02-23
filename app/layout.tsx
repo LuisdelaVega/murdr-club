0d7a42b59b3fa0b4495eefd770ce9aa7fa74099c
LuisdelaVega <luisr.delavega@gmail.com>
2024-02-22 22:11:49 -0500

    Removed unnecessary export in RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Toaster } from "./components/ui/sonner";
 import "./globals.css";
 import { cn } from "./lib/utils";
 
-export const fontSans = FontSans({
+const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
