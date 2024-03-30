67f0109d2f4bc0fd37c0679bb401f9ca8bc24c61
LuisdelaVega <luisr.delavega@gmail.com>
2024-03-30 18:17:26 -0400

    Adding Vercel Web Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import { ThemeProviderWrapper } from "./components/providers/theme-provider-wrapper";
@@ -38,6 +39,7 @@ export default function RootLayout({
           <main>{children}</main>
           <Toaster richColors closeButton />
         </ThemeProviderWrapper>
+        <Analytics />
       </body>
     </html>
   );
